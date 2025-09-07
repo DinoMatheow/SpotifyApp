bfe189df86593f19a8cc3e89b6c6e3ee21f737f9
Matheow <95660245+DinoMatheow@users.noreply.github.com>
2025-09-07 12:16:24 -0500

    Created Component/Player.tsx- Add Icons/Play-Pause

---
+++ src/components/Player.tsx
@@ -0,0 +1,20 @@
+
+
+
+
+export const Player = () => {
+    return (
+        <div className="flex flex-row justify-between w-full px-4 z-50">
+            <div>
+                CurrentSong...
+            </div>
+            <div>
+                Reproductor
+            </div>
+            <div>
+                Volumen
+            </div>
+        </div>
+    )
+
+}
